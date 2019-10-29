f1776645524be1aa99ae2523dda65877a472bb14
Ssong10 <leesy1403@naver.com>
2019-10-29 10:24:59 +0900

    02 | Music index,detail

--- musics/views.py
+++ musics/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Music
+from .serializers import MusicSerializers
 # Create your views here.
+
+@api_view(['GET']) # HTTP method -> GET
+def index(request):
+    musics = Music.objects.all()
+    serializer = MusicSerializers(musics, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def detail(request,music_pk):
+    music = get_object_or_404(Music,pk=music_pk)
+    serializer = MusicSerializers(music)
+    return Response(serializer.data)
